c99e9b4d54a0f50fef8217b95d174f46351ced9f
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-09-28 15:50:20 +0530

    pattern

---
+++ pattern/p8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'A'+i;
+        for (int j = i+1; j >=0; j--)
+        {
+            cout << ch << " ";
+            ch = ch + 1;
+        }
+        cout <<endl;
+    }
+    return 0;
+}
