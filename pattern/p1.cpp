8acd2aba3306c237537f343c75fe6d9b646a1979
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-09-28 10:28:24 +0530

    pattern

---
+++ pattern/p1.cpp
@@ -0,0 +1,25 @@
+//pattern printing square pattern
+
+// * * * * 
+// * * * *
+// * * * *
+// * * * *
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << "* ";
+        }
+    cout << endl;
+        
+    }
+    return 0;
+}
