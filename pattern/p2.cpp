aeb68c39698f383a120cb9a50afc4f12c28da86b
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-09-28 10:33:33 +0530

    pattern

---
+++ pattern/p2.cpp
@@ -0,0 +1,24 @@
+// A B C D 
+// A B C D
+// A B C D
+// A B C D
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'A';
+        
+        for (int j = 0; j < n; j++)
+        {
+            cout << ch << " ";
+            ch = ch +1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
