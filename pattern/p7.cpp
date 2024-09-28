d40a1e72c0331ab70da4e590e3d8e057eacc111c
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-09-28 15:34:23 +0530

    pattern

--- pattern/p7.cpp
+++ pattern/p7.cpp
@@ -5,14 +5,15 @@ using namespace std;
 
 int main() {
     int n = 4;
-    int num = 1;
+    //int num = 1;
+    char ch = 'A';
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i+1; j++)
+        for (int j = i+1; j > 0; j--)
         {
-            cout << num << " ";
-            num++;
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
