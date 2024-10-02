14b0c3dfc1c6d77f92c3bb515ef8c3a835a1c3e4
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-10-02 11:20:13 +0530

    Array

---
+++ array/a1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[5] = {99, 45, 34, 80, 75};
+
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+    cout << marks[4] << endl;
+
+    return 0;
+}
