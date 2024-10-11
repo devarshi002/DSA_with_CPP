abbbfbc858c92c69ef63d3af63e89925096d6725
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-10-11 09:44:05 +0530

    vector

---
+++ vector/v5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> vec;
+    vec.push_back(20);               // push_back function
+    vec.push_back(30);
+    vec.push_back(40); 
+    cout << "Size = " << vec.size() << endl;   // size of fuction to check size of vector
+    vec.pop_back();   //pop_back fucn to pull out the element from vector
+    for(int i : vec) {
+        cout << i << endl;
+    }
+    return 0;
+}
