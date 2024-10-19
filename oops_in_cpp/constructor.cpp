a5c7919d58755a2b96d9d945572804ffc2dacb1c
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-10-19 11:45:28 +0530

    constructor

--- oops_in_cpp/constructor.cpp
+++ oops_in_cpp/constructor.cpp
@@ -5,8 +5,18 @@ using namespace std;
 
 class Teacher {
 public:
-    Teacher() {
-        dept = "Computer science";
+    // non-parameterize consturctor
+    // Teacher() {
+    //     dept = "Computer science";
+    // }
+
+
+    // parameterized constructor
+    Teacher(string n, string d, string s, double sal) {
+        name = n;
+        dept = d;
+        subject = s;
+        salary = sal;
     }
     string name;
     string dept;
@@ -14,19 +24,25 @@ public:
     double salary;
 
 
-    void changeDept(string newDept) {
-        dept=newDept;
-    }
+    // void changeDept(string newDept) {
+    //     dept=newDept;
+    // }
+
+    void getinfo() {
+        cout << "Name: " << name << endl;
+        cout << "Dept: " << dept << endl;
+    };
 };
 
 
 
 int main() {
-    Teacher t1;
-    t1.name = "Devarshi Tambulkar";
-    t1.subject = "C++";
-    t1.salary = 50000;
+    Teacher t1("Devarshi","computer engineering","C++",90000);
+    // t1.name = "Devarshi Tambulkar";
+    // t1.subject = "C++";
+    // t1.salary = 50000;
 
-    cout << t1.dept << endl;
+    // cout << t1.dept << endl;
+    t1.getinfo();
     return 0;
 }
