4bff9b315c566d13889a4f8d730db1d30fd1c1b2
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-11-08 11:05:35 +0530

    constructor

--- oops_in_cpp/constructor1.cpp
+++ oops_in_cpp/constructor1.cpp
@@ -5,14 +5,22 @@ using namespace std;
 class Example {
     public:
        int value;
+        // Default constructor
        Example() {
         value = 0;
         cout << "Default constructor called!" << endl;
        }
+
+       // parameterized constructor
+       Example(int x) {
+        value = x;
+        cout << "Parameterized constructor called with value: " << value << endl;
+       };
 };
 
 int main() {
     Example ex1;
+    Example ex2(10);
 
     return 0;
 };
